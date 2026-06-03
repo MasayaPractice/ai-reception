89dfbf9205b11d0357fb56c51515b3509bf13b46
MasayaPractice <forpracticemasaya@gmail.com>
2026-06-03 11:55:59 +0200

    fix: caption重複削除

--- pages/reception_appt.py
+++ pages/reception_appt.py
@@ -44,8 +44,6 @@ def render_reception_appt() -> None:
 
         register_face = st.checkbox("📷　顔画像登録がまだのお客様はこちらをチェックして登録をお願いします", key="appt_register_face_check")
         st.caption("※ 任意・次回から自動受付できます")
-        st.caption("※ 任意・次回から自動受付できます")
-        st.caption("※ 任意・次回から自動受付できます")
         st.markdown('</div>', unsafe_allow_html=True)
 
     if register_face:
